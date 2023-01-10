625acf09f1197514543d436a1479aec3affc9d66
User™ <User@LAPTOP-V31L4V20>
2023-01-10 21:07:34 +0300

    Добавьте файлы проекта.

---
+++ Karkas_Korizhin_3-2_IS/Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Karkas_Korizhin_3_2_IS
+{
+    internal class Manager
+    {
+        public static Frame MainFrame { get; set; }
+
+    }
+}
